883686a23329e50efc45b94191b2bcbeecedc483
raydNDevelopment <39228636+raydNDev@users.noreply.github.com>
2020-01-09 14:37:33 +0100

    Add files via upload

--- BerkshireApocalypse/Client/Client/cWeaponskills.lua
+++ BerkshireApocalypse/Client/Client/cWeaponskills.lua
@@ -10,7 +10,7 @@ Weaponskills = {state = false, weapon = nil};
 function Weaponskills.dxDraw()
 	if(isWindowOpen())then
 		dxDrawRectangle(1160*(x/1440), 242*(y/900), 270*(x/1440), 42*(y/900), tocolor(17, 17, 17, 200), false)
-        dxDrawText(getWeaponNameFromID(getPedWeapon(localPlayer)).." Skill: "..math.floor(getElementData(localPlayer,Weaponskills.weapon)).."/1000", 1160*(x/1440), 242*(y/900), 1430*(x/1440), 284*(y/900), tocolor(255, 255, 255, 255), 1.00*(x/1440), NewFont, "center", "center", false, false, false, false, false)
+        dxDrawText(getWeaponNameFromID(getPedWeapon(localPlayer)).." Skill: "..math.floor(getElementData(localPlayer,Weaponskills.weapon)).."/1000", 1160*(x/1440), 242*(y/900), 1430*(x/1440), 284*(y/900), tocolor(255, 255, 255, 255), 1.00, NewFont, "center", "center", false, false, false, false, false)
 	end
 end
 
