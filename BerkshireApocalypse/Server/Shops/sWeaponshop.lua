da310de67d1c8335606b46dffa5dc7d69fdb12b4
raydNDevelopment <39228636+raydNDev@users.noreply.github.com>
2020-01-09 14:42:51 +0100

    Add files via upload

--- BerkshireApocalypse/Server/Shops/sWeaponshop.lua
+++ BerkshireApocalypse/Server/Shops/sWeaponshop.lua
@@ -27,6 +27,9 @@ Weaponshop = {
 	["Grenade"] = {40000,16,25,50,8},
 	["Molotov"] = {40000,18,25,50,8},
 	["Satchel"] = {50000,39,30,75,8},
+	["AvailableWeapons"] = {
+		"Colt 45","Silenced","Desert Eagle","Shotgun","Sawed-off","Combat Shotgun","Uzi","Mp5","Tec-9","AK-47","M4","Rifle","Sniper","Rocket Launcher","Rocket Launcher HS","Flamethrower","Minigun","Grenade","Molotov","Satchel"
+	},
 };
 
 addEvent("Weaponshop.buy",true)
@@ -44,3 +47,14 @@ addEventHandler("Weaponshop.buy",root,function(weapon)
 		else infobox(client,"You don't have enough money!",255,0,0)end
 	else infobox(client,"You have to be at least level "..Weaponshop[weapon][4].." to buy this weapon!",255,0,0)end
 end)
+
+function Weaponshop.giveRandomWeapon(player)
+	local rnd = math.random(1,#Weaponshop["AvailableWeapons"]);
+	local weapon = Weaponshop["AvailableWeapons"][rnd];
+	local handWeapon = getPedWeapon(player,Weaponshop[weapon][5]);
+	if(handWeapon and handWeapon ~= Weaponshop[weapon][2])then
+		takeWeapon(player,handWeapon);
+	end
+	giveWeapon(player,Weaponshop[weapon][2],Weaponshop[weapon][3],true);
+	infobox(player,"You got a weapon.",0,255,0);
+end
